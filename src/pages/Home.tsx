bb734c6feeb8aa8e4c2de57bb2facbbf89ee4eff
yurii.krymskyi <yurii.krymskyi@gmail.com>
2025-10-10 15:56:04 +0200

    fix layout header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,7 @@
-import CustomBlueButton from "../components/ui/CustomBlueButton";
-
 const Home = () => {
   return (
-    <div className="flex flex-row items-center h-[80px] justify-between px-15">
-      <div className="flex flex-row items-center gap-2">
-        <img src="/images/burger-menu.svg" alt="burger-menu" className="w-[50px] cursor-pointer" />
-        <p className="uppercase titles text-[#1B1B1B] font-bold text-[18px] cursor-pointer">Menu</p>
-      </div>
-      <img src="/images/logo.svg" alt="logo" className="w-[250px] cursor-pointer" />
-      <div className="flex flex-row items-center gap-5">
-        <div className="flex flex-row items-center gap-1">
-          <p className="titles text-[#1B1B1B] font-bold text-[18px]">
-            EN
-          </p>
-          <img src="/images/arrow-lang.svg" alt="arrow-lang" className="cursor-pointer" />
-        </div>
-        <CustomBlueButton
-          text="Book Now"
-          className="h-[60px] w-[200px]"
-        />
-      </div>
+    <div>
+
     </div>
   );
 };
