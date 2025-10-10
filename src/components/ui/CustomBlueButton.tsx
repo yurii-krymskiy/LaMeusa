ac20bc39864aa2e88713209a7d0543a555a9bf5f
yurii.krymskyi <yurii.krymskyi@gmail.com>
2025-10-10 15:53:53 +0200

    header

---
+++ src/components/ui/CustomBlueButton.tsx
@@ -0,0 +1,9 @@
+const CustomBlueButton = ({ text, className }: { text: string, className: string }) => {
+  return (
+    <button className={`bg-[#2E3E7B] titles cursor-pointer text-[#FFFFFF] uppercase font-bold text-[18px] ${className} hover:bg-[#3F4F8F] transition duration-200 ease-in-out hover:scale-105 active:scale-95`}>
+      {text}
+    </button>
+  );
+};
+
+export default CustomBlueButton;
